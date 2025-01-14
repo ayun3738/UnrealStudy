a407b547928754e9dce5d79551c85c8eefd8dfc2
ayun3738 <ayun3738@gmail.com>
2025-01-15 01:53:28 +0900

    250115

---
+++ 12. 브루트 포스/영화감독 숌.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+class Shom666{
+    public static void shom666(string[] args)
+    {
+        // ReadLine()에 null이 들어올 수 있으므로 string?으로 선언
+        string? N = Console.ReadLine();
+        if (N == null)
+        {
+            return;
+        }
+        int cnt = int.Parse(N);
+        int num = 665;
+        
+        while (cnt > 0)
+        {
+            num++;
+            if (num.ToString().Contains("666"))
+            {
+                cnt--;
+            }
+        }
+        Console.WriteLine(num);
+    }}
+
---
+++ 12. 브루트 포스/체스판 다시 칠하기.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+
+class ChessColor{
+    public static void chesscolor(string[] args)
+    {
+        // ReadLine()에 null이 들어올 수 있으므로 string?으로 선언
+        string? NM = Console.ReadLine();
+        if (NM == null)
+        {
+            return;
+        }
+        List<int> NMList = NM.Split(' ').Select(s => int.Parse(s)).ToList();
+        int odd = 0;
+        int even = 0;
+        for (int i = 0; i < NMList[0]; i++)
+        {
+            string? line = Console.ReadLine();
+            if (line == null)
+            {
+                return;
+            }
+            
+            for (int j = 0; j < NMList[1]; j++)
+            {
+                if (line[j] == 'B')
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        even++;
+                    }
+                    else
+                    {
+                        odd++;
+                    }
+                }
+        }
+        int tmp1 = NMList[0] * NMList[1] / 2 + 1 - even + odd;
+        int tmp2 = NMList[0] * NMList[1] / 2 - odd + even;
+        Console.WriteLine(Math.Min(tmp1, tmp2));
+        }}
+}
+
